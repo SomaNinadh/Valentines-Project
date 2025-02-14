0a5607f834c6c7a6e669daeb06fbada4cd5a5346
SomaNinadh <104094711+SomaNinadh@users.noreply.github.com>
2025-02-14 14:52:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ const messages = [
     "Really sure??",
     "Are you positive?",
     "Pookie please...",
-    "Just think about it!",
+    "Just think about it! Last chance",
     "If you say no, I will be really sad...",
     "I will be very sad...",
     "I will be very very very sad...",
